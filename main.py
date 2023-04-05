351e058b6cddc22c7e140a5581e5b26fbff6de0e
NIU1639390 <91468245+NIU1639390@users.noreply.github.com>
2023-04-05 19:18:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,42 +21,44 @@ try:
         print("S'ha eliminat correctament la base de dades", sys.argv[5])
 except:
     pass
+try:
+    file = sys.argv[2]
+    if file is not None:
+        coll_pub = pd.read_excel(file, sheet_name="Colleccions-Publicacions")
+        coll_pub = coll_pub.to_json(orient='records')
+        coll_pubjson = json.loads(coll_pub)
+        try:
+            coll = bd.create_collection("coll_pub")
+        except:
+            coll = bd["coll_pub"]
+        for doc in coll_pubjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
 
-file = sys.argv[2]
-if file is not None:
-    coll_pub = pd.read_excel(file, sheet_name="Colleccions-Publicacions")
-    coll_pub = coll_pub.to_json(orient='records')
-    coll_pubjson = json.loads(coll_pub)
-    try:
-        coll = bd.create_collection("coll_pub")
-    except:
-        coll = bd["coll_pub"]
-    for doc in coll_pubjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
+        personatges = pd.read_excel(file, sheet_name="Personatges")
+        personatges = personatges.to_json(orient='records')
+        personatgesjson = json.loads(personatges)
+        try:
+            coll = bd.create_collection("personatges")
+        except:
+            coll = bd["personatges"]
+        for doc in personatgesjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
 
-    personatges = pd.read_excel(file, sheet_name="Personatges")
-    personatges = personatges.to_json(orient='records')
-    personatgesjson = json.loads(personatges)
-    try:
-        coll = bd.create_collection("personatges")
-    except:
-        coll = bd["personatges"]
-    for doc in personatgesjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
+        artistes = pd.read_excel(file, sheet_name="Artistes")
+        artistes = artistes.to_json(orient='records')
+        artistesjson = json.loads(artistes)
+        try:
+            coll = bd.create_collection("artistes")
+        except:
+            coll = bd["artistes"]
+        for doc in artistesjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
 
-    artistes = pd.read_excel(file, sheet_name="Artistes")
-    artistes = artistes.to_json(orient='records')
-    artistesjson = json.loads(artistes)
-    try:
-        coll = bd.create_collection("artistes")
-    except:
-        coll = bd["artistes"]
-    for doc in artistesjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
-    
-    print("S'han insertat totes les dades correctament a la base de dades.")
-
+        print("S'han insertat totes les dades correctament a la base de dades.")
+except:
+    print("No has passat cap arxiu de dades o el tipus és incorrecte")
+        
 conn.close()
