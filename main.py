514d5318eda7e867a785feb4c0806348aa7d4692
MrPauete29 <129769664+MrPauete29@users.noreply.github.com>
2023-04-05 19:06:36 +0200

    Ahora comprueba si se han dado todos los argumentos o solo los primeros

--- main.py
+++ main.py
@@ -14,43 +14,47 @@ DSN = "mongodb://{}:{}".format(Host, Port)
 
 conn = MongoClient(DSN)
 bd = conn["projecte"]
+try:
+    if sys.argv[3] == "--delete_all" and sys.argv[4] == "--bd":
+        conn.drop_database(sys.argv[5])
+except:
+    pass
+try:
+    file = sys.argv[2]
+    if file is not None:
+        coll_pub = pd.read_excel(file, sheet_name="Colleccions-Publicacions")
+        coll_pub = coll_pub.to_json(orient='records')
+        coll_pubjson = json.loads(coll_pub)
+        try:
+            coll = bd.create_collection("coll_pub")
+        except:
+            coll = bd["coll_pub"]
+        for doc in coll_pubjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
 
-if sys.argv[3] == "--delete_all" and sys.argv[4] == "--bd":
-    conn.drop_database(sys.argv[5])
+        personatges = pd.read_excel(file, sheet_name="Personatges")
+        personatges = personatges.to_json(orient='records')
+        personatgesjson = json.loads(personatges)
+        try:
+            coll = bd.create_collection("personatges")
+        except:
+            coll = bd["personatges"]
+        for doc in personatgesjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
 
-file = sys.argv[2]
-if file is not None:
-    coll_pub = pd.read_excel(file, sheet_name="Colleccions-Publicacions")
-    coll_pub = coll_pub.to_json(orient='records')
-    coll_pubjson = json.loads(coll_pub)
-    try:
-        coll = bd.create_collection("coll_pub")
-    except:
-        coll = bd["coll_pub"]
-    for doc in coll_pubjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
-
-    personatges = pd.read_excel(file, sheet_name="Personatges")
-    personatges = personatges.to_json(orient='records')
-    personatgesjson = json.loads(personatges)
-    try:
-        coll = bd.create_collection("personatges")
-    except:
-        coll = bd["personatges"]
-    for doc in personatgesjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
-
-    artistes = pd.read_excel(file, sheet_name="Artistes")
-    artistes = artistes.to_json(orient='records')
-    artistesjson = json.loads(artistes)
-    try:
-        coll = bd.create_collection("artistes")
-    except:
-        coll = bd["artistes"]
-    for doc in artistesjson:
-        if coll.find_one(doc) is None:
-            coll.insert_one(doc)
+        artistes = pd.read_excel(file, sheet_name="Artistes")
+        artistes = artistes.to_json(orient='records')
+        artistesjson = json.loads(artistes)
+        try:
+            coll = bd.create_collection("artistes")
+        except:
+            coll = bd["artistes"]
+        for doc in artistesjson:
+            if coll.find_one(doc) is None:
+                coll.insert_one(doc)
+except:
+    print("No has passat cap arxiu de dades o el tipus no es el correcte")
 
 conn.close()
